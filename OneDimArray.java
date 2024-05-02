2d4aeb97c32b82626f9b0f3e9c34d633854c771a
SiddannaMane <164479024+SiddannaMane@users.noreply.github.com>
2024-05-02 13:12:11 +0530

    Add files via upload

---
+++ OneDimArray.java
@@ -0,0 +1,15 @@
+class MultiDimArry {
+    public static void main(String[] args) {
+        int arr[][]={{2,3,4},{5,6,0},{9,7,3}};
+        System.out.println("elements is a 2D arryay");
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.println(arr[i][j]+"");
+            }
+            System.out.println();
+        }
+    }
+}
+     
